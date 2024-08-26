d74c097a5f7f036351d74412235e877c556122ee
gabrielbarauna <gbferreira08@gmail.com>
2024-08-26 15:52:17 -0300

    feat: add components and create tech component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProjectsComponent } from "./components/projects/projects.component";
+import { TechComponent } from "./components/tech/tech.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProjectsComponent],
+  imports: [RouterOutlet, ProjectsComponent, TechComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'm-portfolio';
+  title = 'my-portfolio';
 
   constructor(){}
 }
