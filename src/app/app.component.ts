6f754b25b747208a1e4ca9b794d3e864a2adb50d
gabrielbarauna <gbferreira08@gmail.com>
2024-08-27 01:51:51 -0300

    feat:add pag apresentation with responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ProjectsComponent } from "./components/projects/projects.component";
 import { TechComponent } from "./components/tech/tech.component";
 import { AboutMeComponent } from "./components/about-me/about-me.component";
+import { PresentationComponent } from "./components/presentation/presentation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProjectsComponent, TechComponent, AboutMeComponent],
+  imports: [RouterOutlet, ProjectsComponent, TechComponent, AboutMeComponent, PresentationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
