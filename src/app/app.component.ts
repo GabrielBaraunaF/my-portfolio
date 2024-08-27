0b3916abc395ffd45fd20d53e674b76d5c9fbd87
gabrielbarauna <gbferreira08@gmail.com>
2024-08-27 00:35:12 -0300

    feat: add component about me and style css for responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProjectsComponent } from "./components/projects/projects.component";
 import { TechComponent } from "./components/tech/tech.component";
+import { AboutMeComponent } from "./components/about-me/about-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProjectsComponent, TechComponent],
+  imports: [RouterOutlet, ProjectsComponent, TechComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
