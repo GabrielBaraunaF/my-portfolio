9e92197f09330bda8c5a6ac2333f8f498e5970b2
gabrielbarauna <gbferreira08@gmail.com>
2024-08-28 02:30:14 -0300

    feat: add navbar and fix styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { ProjectsComponent } from "./components/projects/projects.component";
 import { TechComponent } from "./components/tech/tech.component";
 import { AboutMeComponent } from "./components/about-me/about-me.component";
 import { PresentationComponent } from "./components/presentation/presentation.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProjectsComponent, TechComponent, AboutMeComponent, PresentationComponent],
+  imports: [RouterOutlet, ProjectsComponent, TechComponent, AboutMeComponent, PresentationComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -16,4 +17,12 @@ export class AppComponent {
   title = 'my-portfolio';
 
   constructor(){}
+
+  onScrollIntoView(elem: string) {
+    console.log(elem)
+    const element = document.querySelector(elem);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } 
+  }
 }
