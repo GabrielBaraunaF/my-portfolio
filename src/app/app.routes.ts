5ba4012a347046b9c68562e2a7f8a464f87e845c
gabrielbarauna <gbferreira08@gmail.com>
2024-08-30 04:42:48 -0300

    teste routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    
+    {
+        path: '',
+        component:AppComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
 ];
