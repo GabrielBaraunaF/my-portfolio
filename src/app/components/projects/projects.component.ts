b4410074cdaceb81b8eb5d280eb54fac31936fe2
gabrielbarauna <gbferreira08@gmail.com>
2024-10-01 15:46:47 -0300

    feat:add changes for about-me

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import { AppComponent } from '../../app.component';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { RepoCardComponent } from "../../repo-card/repo-card.component";
 
 
 
@@ -11,7 +12,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [AppComponent,CommonModule],
+  imports: [AppComponent, CommonModule, RepoCardComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
